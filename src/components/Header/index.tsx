8da4d6cc3539f8d577e732829304ee2df0f95930
Alisson de Andrade Araújo <alisson2d@hotmail.com>
2021-04-25 09:55:13 -0300

    Completo!

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -13,7 +13,7 @@ export function Header() {
     <header className={styles.headerContainer}>
       <img src="/logo.svg" alt="Podcastr"/>
 
-      <p>O melhor para você ouvir, sempre ! Acesse: <a href="https://github.com/alissonandrade2020">Github</a> | <a href="https://app.rocketseat.com.br/me/alissondeandradearaujo">Rocketseat - Perfil</a></p>
+      <p>O melhor para você ouvir, sempre !           Acesse: <a href="https://github.com/alissonandrade2020">Github</a> | <a href="https://app.rocketseat.com.br/me/alissondeandradearaujo">Rocketseat - Perfil</a></p>
 
       <span>{currentDate}</span>
     </header>
