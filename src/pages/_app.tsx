6bf52087c13ab10a7b84c95b82f8bec633d3f97f
Alisson de Andrade Araújo <alisson2d@hotmail.com>
2021-04-28 15:37:40 -0300

    Completo.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,35 @@
-import React, { useState } from 'react'
+// ARQUIVO QUE FICA EM TORNO DE TODA A APLICAÇÃO
 
-import { PlayerContextProvider } from '../contexts/PlayerContext'
-import { Header } from '../components/Header'
-import { Player } from '../components/Player'
+// INSERIR AQUI COMPONENTES QUE ESTARÁ PRESENTE EM TODAS AS PÁGINAS
+// DA APLICAÇÃO
 
-import '../styles/global.scss'
+import '../styles/global.scss';
 
-import styles from '../styles/app.module.scss'
+import { useState } from 'react';
+
+import { Header } from '../components/Header';
+import { Player } from '../components/Player';
+
+import styles from '../styles/app.module.scss';
+
+import { PlayerContextProvider } from '../contexts/PlayerContext';
 
 function MyApp({ Component, pageProps }) {
-
-  return (
-    <PlayerContextProvider>
-      <div className={styles.appWrapper}>
-      <main>
-        <Header />
-        <Component {...pageProps} />
-      </main>
-      <Player />
-      </div>
-    </PlayerContextProvider>
-  )
+    return(
+      <PlayerContextProvider>
+        <div className={styles.wrapper}>
+          <main>
+            <Header />
+            <Component {...pageProps} />
+          </main>
+          <Player />
+        </div>
+      </PlayerContextProvider>
+  );
 }
 
 export default MyApp
+
+
+// PlayerContext -> Todos os componentes dentro da tag <PlayerContext> possuem acesso
+// às informações contidas nele
