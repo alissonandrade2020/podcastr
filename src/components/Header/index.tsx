15926bcf7ecd1fb154864088042810031d59bf96
Alisson de Andrade Araújo <alisson2d@hotmail.com>
2021-04-28 15:31:24 -0300

    Completo.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,21 +1,22 @@
-import format from 'date-fns/format';
-import ptBR from 'date-fns/locale/pt-BR';
+import Link from 'next/link'
+import format from 'date-fns/format'
+import ptBR from 'date-fns/locale/pt-BR'
 
-import styles from './styles.module.scss';
+import styles from './styles.module.scss'
 
 export function Header() {
-
-  const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
-    locale: ptBR,
-  })
-
-  return(
-    <header className={styles.headerContainer}>
-      <img src="/logo.svg" alt="Podcastr"/>
-
-      <p>O melhor para você ouvir, sempre ! Acesse: <a href="https://github.com/alissonandrade2020">Github</a> | <a href="https://app.rocketseat.com.br/me/alissondeandradearaujo">Rocketseat - Perfil</a></p>
-
-      <span>{currentDate}</span>
-    </header>
-  );
+    const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
+        locale: ptBR,
+    })
+    return (
+        <header className={styles.headerContainer}>
+            <Link href="/">
+                <a>
+                    <img src='/logo.svg' alt="podcastr" />
+                </a>
+            </Link>
+            <p className={styles.slogan}>O melhor para você ouvir, sempre</p>
+            <span>{currentDate}</span>
+        </header>
+    )
 }
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,6 @@
-import axios from 'axios';
+import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/magaliais/podcastr'
+    baseURL: process.env.API_URL || 'http://localhost:3333/'
 })
+
